f135a68c96d2850b7e143ec08c16cb3fb2848556
Santhosh Reddy <152322093+santhoshreddy824@users.noreply.github.com>
2025-11-17 20:12:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "https://meddirect-project-backend.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
